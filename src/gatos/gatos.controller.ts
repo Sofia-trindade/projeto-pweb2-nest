3c085208b7f96d1f597cf94b6e749308862e4227
sofia trindade <sofiaatrindadee02@gmail.com>
2024-03-05 14:03:17 -0300

    atualizar a rota get/gatos para retorna um array de gatos

--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Gato } from 'src/gato/gato.interface';
 
 @Controller('gatos')
-export class GatosController {}
+export class GatosController {
+
+    private readonly gatos:Gato[]=[
+        {
+            nome: 'pink',
+            raca: 'vira lata',
+            idade: 5
+        },
+        {
+            nome: 'Black',
+            raca: 'vira lata',
+            idade: 1
+        }
+    ];
+
+    @Get()
+    findALL(): Gato[]{
+        return this.gatos;
+    }
+}
